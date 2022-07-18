8c25f5180d09528c491ccbfdd5751b3c04db8c96
Todd McDaniel <lazymutt@mac.com>
2022-07-18 13:21:42 -0700

    added license and header info to source

--- toy_blockchain.py
+++ toy_blockchain.py
@@ -14,6 +14,51 @@ Cleaned up the example and added proof of work
 
 """
 
+# MIT License
+#
+# Copyright (c) 2022 Todd McDaniel
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# toy_blockchain.py ############################################################
+#
+#   A toy blockchain, now with mining, in 71 lines of code.
+#
+#    1.0.0  2022.04.24      Initial release. tjm
+#
+#
+################################################################################
+
+# Notes: #######################################################################
+#
+#     Based on:
+#     https://medium.com/@mukeshmishra0381/create-your-own-blockchain-using-python-7c6a117c5f70
+#
+#     Which itself may or may not be heavily borrowed from:
+#     https://medium.com/@lhartikk/a-blockchain-in-200-lines-of-code-963cc1cc0e54
+#
+#
+#     Cleaned up the example and added proof of work
+#
+################################################################################
+
+
 import argparse
 import datetime
 import hashlib
